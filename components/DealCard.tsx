b4ad8a0c51812ace068faa6fbe368ed2eccd385a
Mathis Witte <mail@mwitte.dev>
2021-04-12 00:55:50 +0200

    Add validFrom information to DealCard
    ValidFrom shows the weekday from which on the deal is available

--- components/DealCard.tsx
+++ components/DealCard.tsx
@@ -3,44 +3,56 @@ import {
   Stack, Text, Image, Skeleton,
 } from '@chakra-ui/react';
 
-const DealCard = ({ deal } : { deal: Deal }) => {
-  const validFrom = deal.validFrom ?? 0;
+const isValidToday = (validFrom: number) => {
   const currentDay = new Date().getDay();
-  return (
-    <Stack
-      borderRadius={6}
-      border="1px solid"
-      borderColor={validFrom <= currentDay ? 'gray.600' : 'red.600'}
-    // height="200px"
-      padding={3}
-    >
-      <Stack justifyContent="center" height="100px">
-
-        <Image
-          alignSelf="center"
-          maxH="100px"
-      // objectFit="cover"
-          fallback={<Skeleton alignSelf="center" h="100px" w="140px" />}
-          src={deal.imageUrl}
-          alt="deal image"
-        />
-      </Stack>
-      <Stack spacing="1px">
-        <Text fontSize="xs" noOfLines={1}>
-          {deal.name}
-        </Text>
-        <Text fontSize="xs">
-          {deal.dealPrice}
-          {' '}
-          €
-        </Text>
-        <Text fontSize="xs">
-          {deal.shop}
-        </Text>
-      </Stack>
-
-    </Stack>
-  );
+  return (validFrom ?? 0) <= currentDay;
 };
+
+const weekDays = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'];
+
+const DealCard = ({ deal } : { deal: Deal }) => (
+  <Stack
+    borderRadius={6}
+    border="1px solid"
+    borderColor={isValidToday(deal.validFrom) ? 'gray.600' : 'red.600'}
+    // height="200px"
+    padding={3}
+  >
+    <Stack justifyContent="center" height="100px">
+
+      <Image
+        alignSelf="center"
+        maxH="100px"
+      // objectFit="cover"
+        fallback={<Skeleton alignSelf="center" h="100px" w="140px" />}
+        src={deal.imageUrl}
+        alt="deal image"
+        onClick={() => window.open(deal.imageUrl, '_blank')}
+      />
+    </Stack>
+    <Stack spacing="1px">
+      <Text fontSize="xs" noOfLines={[3, 1]}>
+        {deal.name}
+      </Text>
+      <Text fontSize="xs" as="i">
+        {deal.unit}
+      </Text>
+      <Text fontSize="xs">
+        {deal.dealPrice}
+        {' '}
+        €
+      </Text>
+      <Text fontSize="xs" casing="capitalize">
+        {deal.shop}
+        {' '}
+        {!isValidToday(deal.validFrom) && (
+          `(gültig ab ${weekDays[deal.validFrom - 1]})`
+        )}
+
+      </Text>
+    </Stack>
+
+  </Stack>
+);
 
 export default DealCard;
