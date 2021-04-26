300b502a3a1bf22895e35a72271b49cdbad52a01
Mathis Witte <mail@mwitte.dev>
2021-04-26 17:53:34 +0200

    Add light-theme selection

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,22 +1,23 @@
 import React, { useContext } from 'react';
 import {
-  Flex, Input, InputGroup, InputLeftElement,
+  Flex, Input, InputGroup, InputLeftElement, useColorMode, Button,
 } from '@chakra-ui/react';
-import { SearchIcon } from '@chakra-ui/icons';
+import { SearchIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
 
 import SearchContext from '../context/SearchContext';
 
 const NavBar = () => {
   const { filter, setFilter } = useContext(SearchContext);
-
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Flex
       as="header"
-      // borderppBottom="1px"
       padding={4}
       position="sticky"
-      justifyContent="center"
+      justifyContent="space-between"
+      marginBottom={2}
     >
+      <div />
       <InputGroup width={400}>
         <InputLeftElement>
           <SearchIcon />
@@ -25,8 +26,14 @@ const NavBar = () => {
           placeholder="Search..."
           value={filter}
           onChange={(event) => setFilter(event.target.value.toLowerCase())}
+          background={colorMode === 'dark' && 'gray.700'}
+          boxShadow="md"
         />
       </InputGroup>
+      <Button onClick={toggleColorMode} alignSelf="flex-end">
+
+        {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+      </Button>
     </Flex>
   );
 };
